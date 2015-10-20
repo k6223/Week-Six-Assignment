b6b66a2c131aa24650c9c767bd8bb4e6a0270771
k6223 <kollin.schalhamer@myemail.indwes.edu>
2015-10-20 21:22:37 +0000

    Added quiz answers and finished life

--- GOF1.py
+++ GOF1.py
@@ -2,26 +2,20 @@
 #Kollin Schalhamer
 #CIS-125
 import random
+def main():
 
-world =[]
-height = 22
-width = 80
+    world =[]
+    height = 22
+    width = 80
 
-#Function populate world
-#Pre conditions
-#World is a list
-
-#Post conditions
-#World will contain random cells
-def populate(stuff, h = height, w = width):
     for x in range(h):
         row = []
-        for y in range(w):
-            r = random.randint(0,1)    
-            if r == 0:
-                row.append("_")
-            else:
-                row.append("*")
+    for y in range(w):
+        r = random.randint(0,1)    
+        if r == 0:
+            row.append("_")
+        else:
+            row.append("*")
         
         stuff.append(row)
 
@@ -37,3 +31,34 @@ def display(petri_dish, h = height, w = width):
             rowString += petri_dish[x][y]
         worldString += rowString +"\n"
     print(worldString)
+    
+
+    populate(world,h,w)
+    display(world,h,w)
+    
+    userIn = input("Press any key, Q stops the program: ")
+    while userIn != "Q":
+        
+        display (world,h,w)
+        generation(world,h,w)
+        
+        UserIn = input("Press a key, Q stops the program: ")
+    print("Goodbye!")
+#This function populates the world    
+    
+def populate(world,h,w):
+    
+    pass 
+#this function displays the world
+
+def display(world,h,w):
+    
+    
+    pass
+#this function creates a new world for each generation and decided on whether cells live or die
+def generation(world,h,w):
+    pass
+
+main() 
+
+    
